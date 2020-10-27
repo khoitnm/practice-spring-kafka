4b5cf2b8c3ea42ad246e55bf2c9c168206be20d7
Kevin Tran <kevin.tran@leonardo.com>
2020-10-26 22:52:03 -0400

    Remove maven-comipler-plugin so that it can build the project successfully.

--- pro-01-simple/simple-producer/src/main/java/org/tnmk/pro01simple/sample/person/Initiation.java
+++ pro-01-simple/simple-producer/src/main/java/org/tnmk/pro01simple/sample/person/Initiation.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-import org.tnmk.practicespringkafka.common.message.protobuf.Person;
 import org.tnmk.pro01simple.sample.person.producer.PersonProducer;
 
 @Service
@@ -13,13 +12,13 @@ public class Initiation {
     @Autowired
     private PersonProducer personProducer;
 
-
     @EventListener(ApplicationReadyEvent.class)
-    public void init(){
-        Person person = Person.newBuilder()
-            .setNickName("PersonV1_"+System.nanoTime())
-            .setRealName("RealName_"+System.nanoTime())
-            .build();
+    public void init() {
+//        Person person = Person.newBuilder()
+//            .setNickName("PersonV1_"+System.nanoTime())
+//            .setRealName("RealName_"+System.nanoTime())
+//            .build();
+        String person = "PersonV1_" + System.nanoTime();
         personProducer.send(person);
     }
 }
--- pro-01-simple/simple-producer/src/main/java/org/tnmk/pro01simple/sample/person/producer/PersonProducer.java
+++ pro-01-simple/simple-producer/src/main/java/org/tnmk/pro01simple/sample/person/producer/PersonProducer.java
@@ -1,7 +1,5 @@
 package org.tnmk.pro01simple.sample.person.producer;
 
-import com.google.protobuf.GeneratedMessageV3;
-import org.tnmk.practicespringkafka.common.message.protobuf.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +12,11 @@ public class PersonProducer {
     private static final Logger logger = LoggerFactory.getLogger(PersonProducer.class);
 
     @Autowired
-    private KafkaTemplate<String, GeneratedMessageV3> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     private String topic = TopicConstants.PERSON;
 
-    public void send(Person data){
+    public void send(String data){
         logger.info("[KAFKA PUBLISHER] sending data='{}' to topic='{}'", data, topic);
 
         kafkaTemplate.send(topic, data);
