b70a32cd60e4af6427caf89a58027a3796d4513f
Kevin Tran <kevin.tran@leonardo.com>
2020-10-27 09:35:13 -0400

    Update Kafka configuration.

--- kafka-docker/many-brokers_start.sh
+++ kafka-docker/many-brokers_start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+# KAFKA_ADVERTISED_HOST_NAME: 192.168.1.2 (producers & consumers should be able to connect via KAFKA_ADVERTISED_HOST_NAME:9092)
+# ZOOKEEPER_CONNECT: zookeeper:2181
 docker-compose up -d zookeeper
 docker-compose scale kafka=2
--- kafka-docker/simple_start.sh
+++ kafka-docker/simple_start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# KAFKA_ADVERTISED_HOST_NAME: localhost (producers & consumers should be able to connect via KAFKA_ADVERTISED_HOST_NAME:9092)
+# ZOOKEEPER_CONNECT: zookeeper:2181
+#   Problem: producer can connect to brokers, but there's no leader.
 docker-compose -f docker-compose-single-broker.yml up -d
