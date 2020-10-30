febad2d722bf4251d9f105639e522ec0e37945f5
Kevin Tran <kevin.tran@leonardo.com>
2020-10-30 13:37:31 -0400

    Clone module pro-03-json-message

---
+++ pro-03-json-message/pro-03-consumer/src/main/java/org/tnmk/pro03jsonmessage/sample/person/consumer/TopicConstants.java
@@ -0,0 +1,5 @@
+package org.tnmk.pro03jsonmessage.sample.person.consumer;
+
+public class TopicConstants {
+    public static final String PERSON = "person";
+}
---
+++ pro-03-json-message/pro-03-producer/src/main/java/org/tnmk/pro03jsonmessage/sample/person/Pro03JsonMessageProducerApplication.java
@@ -0,0 +1,15 @@
+package org.tnmk.pro03jsonmessage.sample.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot application to test Kafka integration.
+ */
+@SpringBootApplication
+public class Pro03JsonMessageProducerApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Pro03JsonMessageProducerApplication.class, args);
+    }
+}
