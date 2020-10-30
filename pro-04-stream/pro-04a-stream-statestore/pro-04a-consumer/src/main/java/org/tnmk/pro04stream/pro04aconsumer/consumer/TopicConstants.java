4469aaddb1bb211ed0fa1c4b950b9a7ab113467a
Kevin Tran <kevin.tran@leonardo.com>
2020-10-30 14:52:45 -0400

    Clone to pro-04a-stream-statestore.

---
+++ pro-04-stream/pro-04a-stream-statestore/pro-04a-consumer/src/main/java/org/tnmk/pro04stream/pro04aconsumer/consumer/TopicConstants.java
@@ -0,0 +1,5 @@
+package org.tnmk.pro04stream.pro04aconsumer.consumer;
+
+public class TopicConstants {
+    public static final String PERSON = "person";
+}
---
+++ pro-04-stream/pro-04a-stream-statestore/pro-04a-producer/src/main/java/org/tnmk/pro04stream/pro04aproducer/producer/TopicConstants.java
@@ -0,0 +1,5 @@
+package org.tnmk.pro04stream.pro04aproducer.producer;
+
+public class TopicConstants {
+    public static final String PERSON = "person";
+}
