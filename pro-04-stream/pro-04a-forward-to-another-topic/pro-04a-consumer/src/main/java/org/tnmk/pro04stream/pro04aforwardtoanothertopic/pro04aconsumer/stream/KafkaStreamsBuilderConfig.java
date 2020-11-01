46665fb46f8feea4919778fc78d43dbf3e9472b6
Kevin Tran <kevin.tran@leonardo.com>
2020-11-01 09:36:20 -0500

    Configure Kafka Stream in application.yaml instead of using KafkaStreamsBuilderConfig.java

--- pro-04-stream/pro-04a-forward-to-another-topic/pro-04a-consumer/src/main/java/org/tnmk/pro04stream/pro04aforwardtoanothertopic/pro04aconsumer/stream/KafkaStreamsBuilderConfig.java
+++ pro-04-stream/pro-04a-forward-to-another-topic/pro-04a-consumer/src/main/java/org/tnmk/pro04stream/pro04aforwardtoanothertopic/pro04aconsumer/stream/KafkaStreamsBuilderConfig.java
@@ -1,42 +1,35 @@
 package org.tnmk.pro04stream.pro04aforwardtoanothertopic.pro04aconsumer.stream;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.kafka.StreamsBuilderFactoryBeanCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
-import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
-import org.springframework.kafka.config.KafkaStreamsConfiguration;
 
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * More details at https://docs.spring.io/spring-kafka/reference/html/#streams-config
+ */
 @Configuration
 @EnableKafkaStreams
 public class KafkaStreamsBuilderConfig {
 
     public static final Logger logger = LoggerFactory.getLogger(KafkaStreamsBuilderConfig.class);
 
-
     /**
-     * Have to configure serdes manually like this, it doesn't support automatically config yet: https://github.com/quarkusio/quarkus/issues/3202
-     *
+     * With Spring Boot, you can just configure values in application.yaml instead of this manual configuration.
      * @return
      */
-    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-    public KafkaStreamsConfiguration kStreamsConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "pro04a");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
-        return new KafkaStreamsConfiguration(props);
-    }
+//    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+//    public KafkaStreamsConfiguration kStreamsConfigs() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "pro04a");
+//        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+//        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+//        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
+//        return new KafkaStreamsConfiguration(props);
+//    }
 
     @Bean
     public StreamsBuilderFactoryBeanCustomizer customizer() {
